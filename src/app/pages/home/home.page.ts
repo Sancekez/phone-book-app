d59382bef2cd0f0a0cc3c666653b735e694f682e
Sancekez <k_k_007@mail.ru>
2023-10-04 21:08:59 +0600

    feat: add take permission logic on main page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {Contacts } from '@capacitor-community/contacts';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,22 @@ import {Router} from "@angular/router";
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+    public contacts: any[] | undefined;
 
-  constructor(public router: Router) { }
+    constructor(public router: Router) {}
 
-  ngOnInit() {
-  }
-  goAuth() {
-    this.router.navigate(['authentication'])
-  }
+    ngOnInit() {
+        this.printContactsData();
+    }
 
+    printContactsData() {
+        // @ts-ignore
+        Contacts.getContacts().then(result => {
+            console.log(result);
+            for (const contact of result.contacts) {
+                // @ts-ignore
+                this.contacts.push(contact)
+            }
+        });
+    }
 }
